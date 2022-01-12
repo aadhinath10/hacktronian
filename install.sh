155bca903eeade70343611a6de8833f9b7ecac06
Aadhinath <79595721+aadhinath10@users.noreply.github.com>
2022-01-12 22:27:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,8 @@ PURPLE='\e[35m'
 CYAN='\e[36m'
 WHITE='\e[37m'
 NC='\e[0m'
-echo""
-echo -e "${RED}  _     _         ______ _    _ _______ ______   _____  ______  _____        ______   "
-echo " | |   | |  /\   / _____) |  / |_______|_____ \ / ___ \|  ___ \(_____)  /\  |  ___ \  "
-echo " | |__ | | /  \ | /     | | / / _       _____) ) |   | | |   | |  _    /  \ | |   | | "
-echo " |  __)| |/ /\ \| |     | |< < | |     (_____ (| |   | | |   | | | |  / /\ \| |   | | "
-echo " | |   | | |__| | \_____| | \ \| |_____      | | |___| | |   | |_| |_| |__| | |   | | "
-echo " |_|   |_|______|\______)_|  \_)\______)     |_|\_____/|_|   |_(_____)______|_|   |_| "
-echo -e "${RED}                                                      ~ Tools for Hacking by Mr. SAGE ${NC}"
-echo ""
-echo -e "${YELLOW} Twitter.com/thehackingsage | Instagram.com/thehackingsage | Github.com/thehackingsage ${NC} "
+
+echo -e "${RED}                                                      ~ Tools for Hacking by Adhi ${NC}"
 echo ""
 echo "---------------------------------------------------------------------------------------"
 echo ""
@@ -51,7 +43,7 @@ fi
 
 echo "[✔] Installing ...";
 echo "";
-git clone https://github.com/thehackingsage/hacktronian.git "$INSTALL_DIR";
+git clone https://github.com/aadhinath10/hacktronian.git "$INSTALL_DIR";
 echo "#!/bin/bash
 python $INSTALL_DIR/hacktronian.py" '${1+"$@"}' > hacktronian;
 chmod +x hacktronian;
